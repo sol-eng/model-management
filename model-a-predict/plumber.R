97ad7024b8d119909dc4d6cb04927709b5863356
Kristopher Overholt <koverholt@gmail.com>
2019-05-02 10:32:06 -0500

    Initial commit of model training and prediction

---
+++ model-a-predict/plumber.R
@@ -0,0 +1,14 @@
+library(plumber)
+library(xgboost)
+
+bst = xgb.load("model/xgb.model")
+
+#* @apiTitle Financial Model - Payment Default
+
+#* @param input A list of variables
+#* @post /predict
+function(input="1,20000,2,2,1,24,2,2,-1,-1,-2,-2,3913,3102,689,0,0,0,0,689,0,0,0,0") {
+  values <- as.numeric(unlist(strsplit(input, split=",")))
+  test_data <- matrix(values, nrow = 1)
+  pred <- predict(bst, test_data)
+}
