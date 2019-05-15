1da69a3bc0daff1ef4c2d603634b34a5670c4d58
Kristopher Overholt <koverholt@gmail.com>
2019-05-15 00:10:39 -0500

    Update model APIs

--- model-b-predict/plumber.R
+++ model-b-predict/plumber.R
@@ -12,4 +12,6 @@ function(input = "1,20000,2,2,1,24,2,2,-1,-1,-2,-2,3913,3102,689,0,0,0,0,689,0,0
   values <- as.numeric(unlist(strsplit(input, split = ",")))
   test_data <- matrix(values, nrow = 1)
   pred <- predict(bst, test_data)
+  res <- list(prediction = pred,
+              model = "model-b")
 }
